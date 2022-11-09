c142f7e3922430a13a902ea7c1c2e5804c6bc822
Maxim Urschumzew <u.maxim@live.de>
2022-11-09 08:44:02 +0100

    Change dependency to local janus repo.
    Also properly call measurement submission now.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 
 
-use janus_client::{ClientParameters, aggregator_hpke_config, default_http_client};
-use janus_core::message::*;
-use janus_messages::{HpkeConfig, Role};
+use janus_client::{ClientParameters, aggregator_hpke_config, default_http_client, Client};
+use janus_core::{time::RealClock};
+use janus_messages::{HpkeConfig, Role, TaskId, Duration};
 use url::*;
-use anyhow::Result;
+// use anyhow::Result;
 use async_std::future::try_join;
+use prio::vdaf::prio3::Prio3Aes128FixedPointBoundedL2VecSum;
+
+use fixed::types::extra::{U15, U31, U63};
+use fixed::{FixedI16, FixedI32, FixedI64};
 
 
 
@@ -22,6 +26,14 @@ use async_std::future::try_join;
 // 5. RoundData we also get from the controller.
 
 ////////////////////////////////////////////////////
+// Type Parametrization
+//
+// ToDo: remove this, and integrate into runtime parametrization.
+type Fx = FixedI32<U31>;
+type Measurement = Vec<Fx>;
+
+
+////////////////////////////////////////////////////
 // Parametrization
 
 #[derive(Clone)]
@@ -30,6 +42,14 @@ pub struct Locations
     leader: Url,
     helper: Url,
     controller: Url, // the server that controls the learning process
+}
+
+impl Locations
+{
+    fn get_aggregator_endpoints(&self) -> Vec<Url>
+    {
+        vec![self.leader.clone(),self.helper.clone()]
+    }
 }
 
 
@@ -41,9 +61,11 @@ pub struct Locations
 // The settings are transmitted from the controller to the clients.
 // The clients generate a `RoundConfig` from them.
 //
+#[derive(Clone)]
 pub struct RoundSettings
 {
     task_id : TaskId,
+    time_precision : Duration,
     should_request_hpke_config: bool,
 }
 
@@ -64,8 +86,8 @@ pub struct CryptoConfig
 #[derive(Clone)]
 pub struct RoundConfig
 {
-    task_id : TaskId,
-    crypto_config: CryptoConfig,
+    settings: RoundSettings,
+    crypto: CryptoConfig,
 }
 
 ////////////////////////////////////////////////////
@@ -74,6 +96,7 @@ pub struct RoundConfig
 pub struct ClientState_Parametrization
 {
     location: Locations,
+    gradient_len: usize,
 }
 
 pub struct ClientState_Permanent
@@ -102,7 +125,6 @@ pub enum ClientStatePU
 }
 
 
-
 /////////////////////////////////////////////////////////////////////////
 // Step 1, initialization
 //
@@ -117,11 +139,11 @@ async fn get_crypto_config(permanent: &ClientState_Permanent, task_id: TaskId, l
 {
     let c: ClientParameters = ClientParameters::new(
         task_id,
-        vec![l.leader, l.helper],
+        l.get_aggregator_endpoints(),
         Duration::from_seconds(1),
     );
-    let f_leader = aggregator_hpke_config(&c, Role::Leader, task_id, &permanent.http_client);
-    let f_helper = aggregator_hpke_config(&c, Role::Helper, task_id, &permanent.http_client);
+    let f_leader = aggregator_hpke_config(&c, &Role::Leader, &task_id, &permanent.http_client);
+    let f_helper = aggregator_hpke_config(&c, &Role::Helper, &task_id, &permanent.http_client);
 
     let (l, h) = try_join!(f_leader, f_helper).await?;
 
@@ -152,12 +174,13 @@ impl ClientState
             {
                 config: RoundConfig
                 {
-                    task_id: round_settings.task_id,
-                    crypto_config: c,
+                    settings: round_settings,
+                    crypto: c,
                 }
             }
         })
     }
+
     //
     // Generate a round config for the next round.
     //
@@ -172,8 +195,8 @@ impl ClientState
             // we get a new crypto config if we were asked for it
             let c = get_crypto_config(&self.permanent, round_settings.task_id, self.parametrization.location.clone()).await?;
             Ok(RoundConfig {
-                task_id: round_settings.task_id,
-                crypto_config: c,
+                settings: round_settings,
+                crypto: c,
             })
         }
         else
@@ -181,8 +204,8 @@ impl ClientState
             // Copy the old config from the state to the new config.
             // Just change the task_id to the new one from the round_settings.
             Ok(RoundConfig {
-                task_id: round_settings.task_id,
-                crypto_config: self.round.config.crypto_config.clone(),
+                settings: round_settings,
+                crypto: self.round.config.crypto.clone(),
             })
         }
     }
@@ -193,8 +216,35 @@ impl ClientState
         Ok(())
     }
 
-    async fn get__submission_result(&self) -> anyhow::Result<()>
+    ///////////////////////////////////////
+    // Submission
+    async fn get__submission_result(&self, measurement: &Measurement) -> anyhow::Result<()>
     {
+        let num_aggregators = 2;
+        let len = self.parametrization.gradient_len;
+        let vdaf_client : Prio3Aes128FixedPointBoundedL2VecSum<Fx>
+            = Prio3Aes128FixedPointBoundedL2VecSum::new_aes128_fixedpoint_boundedl2_vec_sum(
+                num_aggregators,
+                len
+            )?;
+
+        let parameters = ClientParameters::new(
+            self.round.config.settings.task_id,
+            self.parametrization.location.get_aggregator_endpoints(),
+            self.round.config.settings.time_precision,
+        );
+
+        let client = Client::new(
+            parameters,
+            vdaf_client,
+            RealClock::default(),
+            &self.permanent.http_client,
+            self.round.config.crypto.leaderHpkeConfig.clone(),
+            self.round.config.crypto.helperHpkeConfig.clone(),
+        );
+
+        let () = client.upload(measurement).await?;
+
         Ok(())
     }
 }
@@ -203,25 +253,31 @@ impl ClientState
 // The api to be called from python code.
 //
 
+//
+// Init
+//
 pub fn api_get_new_client_state(p: ClientState_Parametrization) -> ClientStatePU
 {
     ClientStatePU::Parametrization(p)
 }
 
-pub async fn api_submit(s: ClientStatePU, round_settings: RoundSettings, data: ()) -> anyhow::Result<ClientStatePU>
+//
+// Submitting
+//
+pub async fn api_submit(s: ClientStatePU, round_settings: RoundSettings, data: &Measurement) -> anyhow::Result<ClientStatePU>
 {
     match s
     {
-        ClientStatePU::Parametrization(mut parametrization) =>
+        ClientStatePU::Parametrization(parametrization) =>
         {
             let client_state = ClientState::new(parametrization, round_settings).await?;
-            let result = client_state.get__submission_result().await?;
+            let () = client_state.get__submission_result(data).await?;
             Ok(ClientStatePU::ValidState(client_state))
         },
         ClientStatePU::ValidState(mut client_state) =>
         {
             client_state.update__to_next_round_config(round_settings).await?;
-            client_state.get__submission_result().await?;
+            let () = client_state.get__submission_result(data).await?;
             Ok(ClientStatePU::ValidState(client_state))
         },
     }
