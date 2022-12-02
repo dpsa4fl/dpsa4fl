68ddcbf576109e233ca7f5642ec8674f535fcec7
Maxim Urschumzew <u.maxim@live.de>
2022-12-02 11:46:55 +0100

    Fix helper.

--- src/helpers.rs
+++ src/helpers.rs
@@ -12,12 +12,11 @@ pub fn task_id_to_string(task_id: TaskId) -> String
     base64::encode_config(&task_id.get_encoded(), URL_SAFE_NO_PAD)
 }
 
-pub fn task_id_from_string(task_id_base64: String) -> Result<String>
+pub fn task_id_from_string(task_id_base64: String) -> Result<TaskId>
 {
     let task_id_bytes = base64::decode_config(task_id_base64, base64::URL_SAFE_NO_PAD)?;
     let task_id = TaskId::get_decoded(&task_id_bytes)?;
-    let res = base64::encode_config(&task_id.get_encoded(), URL_SAFE_NO_PAD);
-    Ok(res)
+    Ok(task_id)
 }
 
 
