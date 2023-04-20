86e2faddd4e0d916817930ef88630d368105fb87
Maxim Urschumzew <u.maxim@live.de>
2023-04-20 18:31:55 +0200

    Add documentation.

--- src/client.rs
+++ src/client.rs
@@ -1,12 +1,9 @@
-// use crate::core::{CommonState_Parametrization, Fx, Measurement};
 use crate::helpers::task_id_from_string;
 
 use crate::core::CommonStateParametrization;
-// use crate::helpers::task_id_from_string;
 
 use dpsa4fl_janus_tasks::fixed::{FixedTypeTag, IsTagInstance, VecFixedAny};
 use fixed::traits::Fixed;
-// use janus_aggregator::dpsa4fl::core::Locations;
 use anyhow::{anyhow, Result};
 use async_std::future::try_join;
 use dpsa4fl_janus_tasks::core::Locations;
@@ -16,10 +13,6 @@ use janus_core::time::RealClock;
 use janus_messages::{Duration, HpkeConfig, Role, TaskId};
 use prio::field::Field128;
 use prio::flp::types::fixedpoint_l2::compatible_float::CompatibleFloat;
-
-// use janus_client::{aggregator_hpke_config, default_http_client, Client, ClientParameters};
-// use janus_core::time::RealClock;
-// use janus_messages::{Duration, HpkeConfig, Role, TaskId};
 use prio::vdaf::prio3::Prio3Aes128FixedPointBoundedL2VecSum;
 
 const TIME_PRECISION: u64 = 3600;
@@ -35,13 +28,6 @@ const TIME_PRECISION: u64 = 3600;
 // 4. Using the RoundSettings, we can get the RoundConfig from the aggregators.
 //    The RoundConfig allows us to submit our results to the aggregators.
 // 5. RoundData we also get from the controller.
-
-////////////////////////////////////////////////////
-// Type Parametrization
-//
-// ToDo: remove this, and integrate into runtime parametrization.
-// pub type Fx = FixedI32<U31>;
-// pub type Measurement = Vec<Fx>;
 
 ////////////////////////////////////////////////////
 // Settings
@@ -347,14 +333,21 @@ impl ClientState
 // The api to be called from python code.
 //
 
-//
-// Init
-//
+/// Create a new client state.
+///
+/// Here, `p` contains the addresses of the aggregation
+/// servers and the corresponding janus-tasks instances.
+/// Note that the state does not contain all information required for submitting gradients,
+/// as this information can only be gotten on a round-by-round basis, once the task id
+/// for a given round is known.
 pub fn api_new_client_state(p: Locations) -> ClientStatePU
 {
     ClientStatePU::InitState(p)
 }
 
+/// Configure the client state for a given round.
+///
+/// If neccessary, this function will request information such as hpke keys from the aggregators.
 pub async fn api_update_client_round_settings(
     s: &mut ClientStatePU,
     round_settings: RoundSettings,
@@ -377,31 +370,11 @@ pub async fn api_update_client_round_settings(
     Ok(())
 }
 
-//
-// Submitting
-//
-pub async fn api_submit(
-    s: &mut ClientStatePU,
-    round_settings: RoundSettings,
-    data: &VecFixedAny,
-) -> anyhow::Result<()>
-{
-    api_update_client_round_settings(s, round_settings).await?;
-
-    match s
-    {
-        ClientStatePU::InitState(_) =>
-        {
-            Err(anyhow!(""))?;
-        }
-        ClientStatePU::ValidState(ref mut client_state) =>
-        {
-            client_state.get_submission_result(data).await?;
-        }
-    };
-    Ok(())
-}
-
+/// Submit gradients to the aggregators.
+///
+/// Given a client state `s`, round settings describing the current round, and a function `get_data`,
+/// which provides the gradient, this function calls `get_data` with the current parameters and submits
+/// the resulting gradient to the aggregators.
 pub async fn api_submit_with<F: FnOnce(&CommonStateParametrization) -> VecFixedAny>(
     s: &mut ClientStatePU,
     round_settings: RoundSettings,
