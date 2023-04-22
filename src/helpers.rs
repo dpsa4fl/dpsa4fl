5f02e203ea4fc4103b67cb788c2f627626ab20a5
Maxim Urschumzew <u.maxim@live.de>
2023-04-22 08:32:47 +0200

    Readd deleted code because it is used by bindings.

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,8 +1,13 @@
 use anyhow::Result;
+use base64::URL_SAFE_NO_PAD;
 use janus_messages::TaskId;
-use prio::codec::{Decode};
+use prio::codec::{Decode, Encode};
 
-/// Decode a task id from a string in the same format as used by janus.
+pub fn task_id_to_string(task_id: TaskId) -> String
+{
+    base64::encode_config(&task_id.get_encoded(), URL_SAFE_NO_PAD)
+}
+
 pub fn task_id_from_string(task_id_base64: String) -> Result<TaskId>
 {
     let task_id_bytes = base64::decode_config(task_id_base64, base64::URL_SAFE_NO_PAD)?;
